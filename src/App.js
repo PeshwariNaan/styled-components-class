09fb9d5811a3bb96b0074f34ce902ab5d90f59f7
J.D. Browne <PeshwariNaan01@proton.me>
2023-03-25 15:05:04 +0100

    Moved content of app.js with theme to ThemeExample.js. Added Loading spinner.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import GlobalStyles from './global-styles';
-
-const BaseTheme = {
-  color: '#222',
-  background: '#fff',
-};
-const DarkTheme = {
-  color: '#fff',
-  background: '#222',
-};
-
-const Container = styled.div`
-  padding: 2rem;
-  color: ${(props) => props.theme.color};
-  background: ${(props) => props.theme.background};
-`;
+import styled from 'styled-components';
+import Loading from './components/Loading';
 
 function App() {
-  const [baseTheme, setTheme] = useState(true);
-
-  const toggleTheme = () => {
-    setTheme(!baseTheme);
-  };
   return (
-    <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
-      <GlobalStyles />
-      <Container>
-        <h1>Hello world</h1>
-        <button className="btn" onClick={toggleTheme}>
-          Toggle theme
-        </button>
-      </Container>
-    </ThemeProvider>
+    <div style={{ padding: '2rem' }}>
+      <Loading />
+    </div>
   );
 }
 
